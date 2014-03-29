bdd5f0c3345df1dc2f71c137960c37483500c229
Ziemek Wolski <ziemek.wolski@gmail.com>
2014-03-29 12:18:33 -0400

    Added a title to simply player.

--- vendor/assets/javascripts/simple.players.js
+++ vendor/assets/javascripts/simple.players.js
@@ -3,6 +3,7 @@
  *  - http://www.opensource.org/licenses/mit-license.php
  *  - http://www.gnu.org/copyleft/gpl.html
  *
+ * SimplePlayer by: Ziemek Wolski @ziemekwolski
  *
  * Speacial thanks to original Authors of JPlayer and Circle player
  * Mark J Panaghiston @thepag
@@ -31,27 +32,29 @@ var initSimplePlayer = function(selector) {
     var playerSnippetClass = 'cp2_container_' + index;
     var playerSnippetSelector = '#' + playerSnippetClass;
     var playerSnippet = $([
-      '<div id="' + playerSnippetClass + '" class="cp-container">',
-      ' <div class="cp-buffer-holder"> <!-- .cp-gt50 only needed when buffer is > than 50% -->',
-      '   <div class="cp-buffer-1"></div>',
-      '   <div class="cp-buffer-2"></div>',
-      ' </div>',
-      ' <div class="cp-progress-holder"> <!-- .cp-gt50 only needed when progress is > than 50% -->',
-      '   <div class="cp-progress-1"></div>',
-      '   <div class="cp-progress-2"></div>',
-      ' </div>',
-      ' <div class="cp-circle-control"></div>',
-      ' <ul class="cp-controls">',
-      '   <li><a href="#" class="cp-play" tabindex="1">play</a></li>',
+      '<div class="cp-title">' + $(this).text() + '</div>',
+      ' <div id="' + playerSnippetClass + '" class="cp-container">',
+      '  <div class="cp-buffer-holder"> <!-- .cp-gt50 only needed when buffer is > than 50% -->',
+      '    <div class="cp-buffer-1"></div>',
+      '    <div class="cp-buffer-2"></div>',
+      '  </div>',
+      '  <div class="cp-progress-holder"> <!-- .cp-gt50 only needed when progress is > than 50% -->',
+      '    <div class="cp-progress-1"></div>',
+      '    <div class="cp-progress-2"></div>',
+      '  </div>',
+      '  <div class="cp-circle-control"></div>',
+      '  <ul class="cp-controls">',
+      '    <li><a href="#" class="cp-play" tabindex="1">play</a></li>',
           //Needs the inline style here, or jQuery.show() uses display:inline instead of display:block
-      '   <li><a href="#" class="cp-pause" style="display:none;" tabindex="1">pause</a></li>', 
-      ' </ul>',
-      '</div>'
+      '    <li><a href="#" class="cp-pause" style="display:none;" tabindex="1">pause</a></li>', 
+      '  </ul>',
+      ' </div>',
+      '</div>',
     ].join(""));
     var playerFiles = $(this).data();
 
     $('#simple-jplayer').append(playerDiv);
-    $(this).html(playerSnippet);
+    $(this).replaceWith(playerSnippet);
 
     //Add the player
     var myCirclePlayer = new CirclePlayer(palyerSelector, playerFiles, { cssSelectorAncestor: playerSnippetSelector});
